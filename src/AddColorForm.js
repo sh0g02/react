5613f712f26ea59a7ffadc73c236c29ee3179fd5
sh0g02 <sh0g0.tksk@gmail.com>
2021-11-30 23:24:31 +0900

    6.3.2 制御されたコンポーネント

--- src/AddColorForm.js
+++ src/AddColorForm.js
@@ -1,22 +1,59 @@
 import React, {useRef} from "react";
 
+/**
+ * 制御されていないコンポーネントの例
+ * @param onNewColor
+ * @returns {JSX.Element}
+ * @constructor
+ */
+// export default function AddColorForm({ onNewColor = f => f }) {
+//   const txtTitle = useRef();
+//   const hexColor = useRef();
+//
+//   const submit = e => {
+//     e.preventDefault(); // HTMLデフォルトの挙動の防止
+//     const title = txtTitle.current.value;
+//     const color = hexColor.current.value;
+//     onNewColor(title, color);
+//     txtTitle.current.value = '';
+//     hexColor.current.value = '';
+//   };
+//
+//   return (
+//     <form onSubmit={submit}>
+//       <input ref={txtTitle} type="text" placeholder="color title..." required />
+//       <input ref={hexColor} type="color" required />
+//       <button>ADD</button>
+//     </form>
+//   );
+// }
+
 export default function AddColorForm({ onNewColor = f => f }) {
-  const txtTitle = useRef();
-  const hexColor = useRef();
-  
-  const submit = e => {
-    e.preventDefault(); // HTMLデフォルトの挙動の防止
-    const title = txtTitle.current.value;
-    const color = hexColor.current.value;
+  const [title, setTitle] = useState("");
+  const [color, setColor] = useState("#000000");
+
+  const submit = (e) => {
+    e.preventDefault();
     onNewColor(title, color);
-    txtTitle.current.value = '';
-    hexColor.current.value = '';
-  };
+    setTitle("");
+    setColor("");
+  }
 
   return (
     <form onSubmit={submit}>
-      <input ref={txtTitle} type="text" placeholder="color title..." required />
-      <input ref={hexColor} type="color" required />
+      <input
+        value={title}
+        onChange={event => setTitle(event.target.value)} // 入力値が更新される度にstateを更新 -> ユーザの入力とstateを同期する
+        type="text"
+        placeholder="color title..."
+        required
+      />
+      <input
+        value={color}
+        onChange={event => setColor(event.target.value)}
+        type="color"
+        required
+      />
       <button>ADD</button>
     </form>
   );
